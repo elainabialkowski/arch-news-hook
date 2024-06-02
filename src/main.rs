842719de3369b8ccceef05341a92370b687ca0f1
Elaina Bialkowski <elainabialkowski@gmail.com>
2024-06-02 16:19:38 -0400

    Added news site parsing. Filters out results older than your last update. Only shows articles with upgradeable packages in title.

---
+++ src/main.rs
@@ -0,0 +1,127 @@
+use anyhow::Result;
+use chrono::{DateTime, FixedOffset};
+use scraper::{ElementRef, Html, Selector};
+use std::{collections::HashMap, fs::File, io};
+
+use alpm::{Alpm, Ver};
+use alpm_utils::{alpm_with_conf, config::Config, configure_alpm};
+
+#[derive(Debug)]
+struct OverviewRow {
+    date: chrono::DateTime<FixedOffset>,
+    title: String,
+    link: String,
+}
+
+impl OverviewRow {
+    fn new(date: chrono::DateTime<FixedOffset>, title: &str, link: &str) -> Self {
+        Self {
+            date,
+            title: title.to_owned(),
+            link: link.to_owned(),
+        }
+    }
+
+    fn parse_row(element: ElementRef) -> Result<OverviewRow> {
+        let td = Selector::parse("td").unwrap();
+        let a = Selector::parse("a").unwrap();
+
+        let tds = element.select(&td).collect::<Vec<ElementRef>>();
+        if tds.len() > 1 {
+            match tds[0..2] {
+                [date, href] => {
+                    let date = DateTime::parse_from_str(
+                        &format!(
+                            "{}T00:00:00-0000",
+                            date.text().collect::<Vec<&str>>().join("")
+                        ),
+                        "%Y-%m-%dT%H:%M:%S%z",
+                    )?;
+
+                    let title = href.select(&a).collect::<Vec<ElementRef>>()[0]
+                        .text()
+                        .collect::<Vec<&str>>()
+                        .join(" ");
+
+                    let link = format!(
+                        "https://archlinux.org{}",
+                        href.select(&a).collect::<Vec<ElementRef>>()[0]
+                            .attr("href")
+                            .unwrap()
+                    );
+
+                    Ok(OverviewRow::new(date, &title, &link))
+                }
+                _ => Err(anyhow::anyhow!("Could not parse row.")),
+            }
+        } else {
+            Err(anyhow::anyhow!("Row too short."))
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let config = Config::new().unwrap();
+    let local = alpm_with_conf(&config).unwrap();
+
+    let tmp = tempfile::tempdir().unwrap().into_path();
+    let mut remote = Alpm::new("/", tmp.to_str().unwrap()).unwrap();
+    configure_alpm(&mut remote, &config).unwrap();
+
+    remote.syncdbs_mut().update(true).unwrap();
+
+    let remote_pkgs = remote
+        .syncdbs()
+        .iter()
+        .flat_map(|db| db.pkgs())
+        .map(|pkg| (pkg.name(), pkg.version()))
+        .collect::<HashMap<&str, &Ver>>();
+
+    let updates = local
+        .localdb()
+        .pkgs()
+        .iter()
+        .map(|pkg| (pkg.name(), pkg.version()))
+        .filter(|(name, version)| remote_pkgs.get(name) > Some(version))
+        .collect::<HashMap<&str, &Ver>>();
+
+    let log_file = io::read_to_string(File::open(local.logfile().unwrap()).unwrap()).unwrap();
+    let lines = log_file.lines();
+    let last_update = lines
+        .into_iter()
+        .rev()
+        .find(|line| line.contains("pacman -Syu"))
+        .map(|line| line.split_whitespace().collect::<Vec<&str>>()[0])
+        .map(|time| time.trim_matches(|c| c == '[' || c == ']'))
+        .map(|time| chrono::DateTime::parse_from_str(time, "%Y-%m-%dT%H:%M:%S%z"))
+        .unwrap()
+        .unwrap();
+
+    let news = reqwest::get("https://archlinux.org/news")
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let news_html = Html::parse_document(&news);
+    let article_tags = Selector::parse("tbody > tr").unwrap();
+
+    let articles = news_html
+        .select(&article_tags)
+        .map(OverviewRow::parse_row)
+        .filter_map(|row| row.ok())
+        .filter(|article| article.date < last_update)
+        .filter(|article| {
+            updates
+                .keys()
+                .any(|s| article.title.to_lowercase().contains(&s.to_lowercase()))
+        })
+        .collect::<Vec<OverviewRow>>();
+
+    println!(
+        "Here are some news entries that relate to packages you plan on updating: {:#?}",
+        articles
+    )
+}
